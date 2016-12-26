26f0be3559becfe53aa5dd642b894303290020ad
sjy1158 <1124080755@qq.com>
2016-12-26 14:33:09 +0800

    D app

--- app.js
+++
@@ -1,9 +0,0 @@
-var path=require("path");
-var express=require("express");
-var app=express();
-app.use("/",express.static(path.join(__dirname,"views")));
-app.listen(16944,function(){
-	console.log(333);
-})
-
-module.exprots=app;
